30ada976269af0e66a737b02853471814dd9b4a7
Marc Karassev <karassev.marc@etu.unice.fr>
2014-01-23 11:30:00 +0100

    Update LoanTest fonctionnelle

--- Projet-Fin-Semestre/tests/managementsystem/LoanTest.java
+++ Projet-Fin-Semestre/tests/managementsystem/LoanTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 
 import org.junit.Test;
 
@@ -21,23 +22,24 @@ import equipment.Equipment;
  */
 public class LoanTest {
 
-	@SuppressWarnings("deprecation")
 	@Test
 	public void constructorsTest() {
 		HashMap<String, Integer> hm = new HashMap<String, Integer>();
 		hm.put("Ipad3", 3);
 		hm.put("Vengeance2100", 1);
+		HashSet<String> hs = new HashSet<String>();
+		hs.add("Ipad3");
+		hs.add("Vengeance2100");
 		Loan l1 = new Loan();
 		Loan l2 = new Loan(hm, new Period(), new Borrower());
 
 		assertEquals(l1.getStuff().size(), 0);
-		assertEquals(l1.getStuff().keySet().toArray(), new Object[0]);
+		assertEquals(l1.getStuff().keySet().isEmpty(), true);
 		assertEquals(l2.getStuff().size(), 2);
-		assertEquals(l2.getStuff().keySet().toArray(), new Object[] {
-				Model.VENGEANCE2100, Model.IPAD3 });
-		assertEquals(l2.getStuff().get(Model.VENGEANCE2100),
+		assertEquals(l2.getStuff().keySet(), hs);
+		assertEquals(l2.getStuff().get("Vengeance2100"),
 				new ArrayList<Equipment>(1));
-		assertEquals(l2.getStuff().get(Model.IPAD3),
+		assertEquals(l2.getStuff().get("Ipad3"),
 				new ArrayList<Equipment>(3));
 		System.out.println(l1 + "\n");
 		System.out.println(l2);
